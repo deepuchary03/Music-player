e862e466be4e58156be47a6e6fcbea83f1699aae
deepuchary03 <deepuchary03@gmail.com>
2025-01-16 18:09:15 +0530

    mongodb

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const mongoose = require("mongoose"); // to connect mongodb schema
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 const PORT = 5000;
-const DB_URI = "mongodb://localhost:27017/project4"; //mongodb compass
+//const DB_URI = "mongodb://localhost:27017/project4"; //mongodb compass
+const DB_URI =
+  "mongodb+srv://deepuchary03:llEerw1yqjLN6lWS@sg3.xpb79.mongodb.net/project4?retryWrites=true&w=majority&appName=Sg3";
 
 app.use(cors());
 app.use(bodyParser.json()); //JSON
